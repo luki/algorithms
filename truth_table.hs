9c312a30d546f38395e7285a9f328c92a143c1c9
Lukas Mueller <lukasamueller@icloud.com>
2019-11-03 15:40:09 +0100

    Add truth_table.hs

---
+++ truth_table.hs
@@ -0,0 +1,18 @@
+## DESCRIPTION
+## The function h can be used to compare two logical propositions
+
+import Data.Bool (not)
+
+k :: (Bool, Bool) -> Bool
+k (True, True)   = False
+k (True, False)  = True
+k (False, True)  = True
+k (False, False) = False
+
+j = \(a, b) -> (a||b) && ((not a) || (not b))
+
+-- Comparing function
+--h :: (((Bool, Bool) -> Bool), ((Bool, Bool) -> Bool)) -> Bool
+h (f1, f2) = and $ 
+  map (\v -> (f1 v) == (f2 v)) 
+      [(True,True), (True, False), (False, True), (False, False)]
